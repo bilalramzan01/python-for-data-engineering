79b26017cdc81a32373b366ef3ad22d84ee65430
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-06 23:30:35 +0500

    changes

---
+++ 01-Programming-Fundamentals/03-Chapter-Strings/Strings.py
@@ -0,0 +1,15 @@
+#Sequence of character enclosed in Quotes, Its immuteable means cant change in existing string
+
+#Single Line
+A = "Bilal"
+B = 'Bilal'
+#Multi-Line
+
+c = """Bilal is a good boy
+and he is also handsome as well """
+
+print(A,B,c)
+
+# String Slicing
+print(nameshort)
+nameshort = A[0:4]
