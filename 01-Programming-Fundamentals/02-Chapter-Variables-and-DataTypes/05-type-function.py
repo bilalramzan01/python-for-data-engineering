932b1c6df2575a27d3edbb2af17028dfb0f01db9
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-06 19:51:44 +0500

    updated

---
+++ 01-Programming-Fundamentals/02-Chapter-Variables-and-DataTypes/05-type-function.py
@@ -0,0 +1,21 @@
+# Type() Function is used to find the type of the given variable
+
+a=7
+print(type(a))
+
+b=3.4
+print(type(b))
+
+b=True
+t=type(b)
+print(t)
+
+#CONVERSION
+a="3.4"
+b=float(a)
+t=type(b)
+print(b,t)
+
+h=34
+j=str(h)
+print(type(j))
