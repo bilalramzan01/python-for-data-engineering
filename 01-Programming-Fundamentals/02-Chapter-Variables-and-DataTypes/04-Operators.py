633ca23dff166fc40e9f8ae1bcd62b4862d38838
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-06 19:27:48 +0500

    changes

---
+++ 01-Programming-Fundamentals/02-Chapter-Variables-and-DataTypes/04-Operators.py
@@ -0,0 +1,24 @@
+#Arithmetic operator +,-,*,/
+a=2
+b=5
+c=a+b
+print(c)
+
+
+# Assignment operator =,+=,-=
+A = 6 #Assigning 6 in A
+print(A)
+B=3
+B +=4
+print(B)
+
+# Comparison operator ==,<,>,<=,>=,!= ALWAYS RETURN TRUE OR FALSE
+d=6<4
+print(d)
+print(5!=3)
+
+
+# Logical operator AND, OR, NOT
+f=True or False
+print(f)
+ 
