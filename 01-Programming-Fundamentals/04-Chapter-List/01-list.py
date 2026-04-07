243e7b607bbea4ce5d88e516fed1b0be8b1d7229
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-07 21:12:00 +0500

    updated

---
+++ 01-Programming-Fundamentals/04-Chapter-List/01-list.py
@@ -0,0 +1,5 @@
+#Container to store set of different type of values and its immuteable
+
+data = ["Bilal", 5, "Apple",5,"Okay"]
+data[0]="wajeh"
+print(data)
---
+++ 01-Programming-Fundamentals/04-Chapter-List/02-lists-methods.py
@@ -0,0 +1,17 @@
+data = ["Bilal", 3 , "Apple", "Acadmey", 3.6]
+print(data)
+data.append("Wajeh")
+print(data)
+
+l1 = [1,43,22,45,65,43,2,4,]
+l1.sort()
+print(l1)
+
+l2 = [1,2,3,5,6]
+l2.insert(3,4)
+print(l2)
+
+l3 = [1,2,3,5,6]
+value = l3.pop(3)
+print(value)
+print(l3)
