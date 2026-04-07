3e340ce33cef0464633547a85a1adfe6789e1135
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-07 22:11:40 +0500

    Updated

---
+++ 01-Programming-Fundamentals/04-Chapter-List/Practice set/Problem3.py
@@ -0,0 +1,5 @@
+#Check that a tuple cannot be changed in python.
+
+a = (1,2,3,4,5)
+a[1]="Bilal"
+print(a)
---
+++ 01-Programming-Fundamentals/04-Chapter-List/Practice set/Problem4.py
@@ -0,0 +1,4 @@
+#Write a program to sum a list with 4 numbers.
+
+l=[1,3,56,65]
+print(sum(l))
