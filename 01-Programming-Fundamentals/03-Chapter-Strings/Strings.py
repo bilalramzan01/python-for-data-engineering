e9cdc64a3967b7784fbc00e14c346f8e7aa6c5c1
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-07 18:46:01 +0500

    updated

--- 01-Programming-Fundamentals/03-Chapter-Strings/Strings.py
+++ 01-Programming-Fundamentals/03-Chapter-Strings/Strings.py
@@ -11,5 +11,16 @@ and he is also handsome as well """
 print(A,B,c)
 
 # String Slicing
-print(nameshort)
 nameshort = A[0:4]
+nameshort = A[:4] #It will start from 0
+nameshort = A[1:] #It will end at 5
+print(nameshort)
+cha4=A[4]
+print(cha4)
+print(A[3])
+print(A[-3])
+print(A[-5:-1])
+
+#Skip value concept
+h="0123456789"
+print(h[0:9:2])
