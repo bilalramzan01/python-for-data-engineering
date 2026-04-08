79cc21a5213ecbc412341f10ae8080dd58d98b78
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-04-08 22:14:46 +0500

    commit

--- 01-Programming-Fundamentals/04-Chapter-List/01-list.py
+++ 01-Programming-Fundamentals/04-Chapter-List/01-list.py
@@ -1,5 +1,5 @@
 #Container to store set of different type of values and its muteable
 
-data = ["Bilal", 5.88, "Apple",5,"Okay","Saib"]
+data = ["Bilal", 5.88, "Apple",5,"Okay",]
 data[0]="wajeh ul Hassan"
 print(data)
