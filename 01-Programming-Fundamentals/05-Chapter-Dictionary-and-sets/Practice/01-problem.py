a05a6040eda5267d1f3b35fce80184c478637e82
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-12 22:12:32 +0500

    UPDATED

--- 01-Programming-Fundamentals/05-Chapter-Dictionary-and-sets/Practice/01-problem.py
+++ 01-Programming-Fundamentals/05-Chapter-Dictionary-and-sets/Practice/01-problem.py
@@ -1,6 +1,9 @@
 # Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
 
-D = {
-      
-
+words = {
+      "Insan": "Human",
+      "Bada": "Large",
+      "Walid": "Father",
 }
+word=input("Enter the word:")
+print(words[word])
